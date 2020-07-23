9000c63d8d6bc43fb8dbcab489df2054c454fa3b
Soujanya Nekkanti <soujanya.nekkenti@gmail.com>
2020-07-23 13:16:16 +0200

    Install gatsby-source-file-system plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,8 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
-// module.exports = {
-//   /* Your site config here */
-//   plugins: [],
-
-// }
-
 module.exports = {
   siteMetadata: {
     title: "Soujanya Nekkanti",
+    description:
+      "Personal website, my resume, skills and interests along with a blog about web development",
     author: "Soujanya Nekkanti",
     twitter: "https://twitter.com/soujanya1604",
     linkedin: "https://www.linkedin.com/in/lakshmisowjanyanekkanti1604/",
@@ -21,6 +11,27 @@ module.exports = {
   plugins: [
     {
       resolve: "gatsby-plugin-styled-components",
+    },
+    {
+      resolve: "gatsby-source-file-system",
+      options: {
+        name: "pages",
+        path: "${_dirname}/src/pages",
+      },
+    },
+    {
+      resolve: "gatsby-source-file-system",
+      options: {
+        name: "posts",
+        path: "${_dirname}/src/posts",
+      },
+    },
+    {
+      resolve: "gatsby-source-file-system",
+      options: {
+        name: "images",
+        path: "${_dirname}/src/images",
+      },
     },
   ],
 }
